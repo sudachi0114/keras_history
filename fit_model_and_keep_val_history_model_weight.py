85622ca3b0eb1daceb0083214a0752f572f5b60a
suzuki daichi <sudachi@DaiMac.local>
2019-07-13 21:40:53 +0900

    <fit_model_and_keep_val_history_model_weight.py> という長いファイルを作成しました。これは Keras の Sequential() で作ったモデルを fit(学習) させ, 学習結果である history(object -> pickle) と、model(json), weights(hdf5) を保存するプログラムです。

---
+++ fit_model_and_keep_val_history_model_weight.py
@@ -0,0 +1,93 @@
+
+# Thinkpad で学習をさせることも出てきたので、
+#   Keras の model.fit で行った学習の log を取得したい。
+
+#   => Keras の model.fit で行った学習の log を取得したい。
+#       log として取るもの : 
+#               history (history object -> pickle ファイル)
+#               model (json 形式)
+#               weight (hdf5 形式)
+
+""" 現状
+    使っている module
+        Keras :
+            model = Sequential()
+                history = model.fit()
+            => history 型 objett が return されるらしい。
+"""
+
+# CIFAR10 だと重いので、 MNIST を例にやってみる。
+#   モデルが学習した結果(変数) を保持したままにしたい。(プログラム外から参照できるようにする。)
+
+from load_prep_minst import load_and_prep_minstData
+from build_model import build_multilayer_perceptron
+#from keras.callbacks import EarlyStopping
+from keras.optimizers import Adam
+
+# -- load & prep --
+(x_train, y_train), (x_test, y_test) = load_and_prep_minstData()
+
+# -- build model --
+model = build_multilayer_perceptron()
+
+# -- compile model --
+model.compile(
+                loss='categorical_crossentropy',
+                optimizer=Adam(),
+                metrics=['accuracy']
+            )
+
+# -- Early Stopping --
+#early_stopping = EarlyStopping(patience=0, verbose=1)
+
+#model.summary()
+
+# -- fit --
+history = model.fit(
+                        x_train, y_train,
+                        batch_size=32,
+                        #epochs=10,
+                        epochs=3,
+                        verbose=1,
+                        validation_split=0.1
+                        #,callbacks=[early_stopping]
+                        )
+
+
+#print(type(history))  # <class 'keras.callbacks.History'>
+#print(history)  # <keras.callbacks.History object at 0xb36762f98>
+
+""" fit() method の戻り値:
+    History オブジェクト． 
+        History.history 属性は
+            * 実行に成功したエポックにおける訓練の損失値と
+            * 評価関数値の記録と，
+            * (適用可能ならば) 検証における損失値と評価関数値
+        も記録しています．
+"""
+
+# https://qiita.com/karadaharu/items/948e4d313fbaa32e408c
+
+# log として取るもの : (1)
+#   history (history object -> pickle ファイル)
+import pickle, datetime
+
+now = datetime.datetime.now()
+
+with open('history_{0:%y%m%d}_{1:%H%M%S}.pkl'.format(now, now), 'wb') as f:
+    pickle.dump(history, f)
+
+# log として取るもの : (2)
+#   model (json 形式)
+import json
+
+model_to_json = model.to_json()
+with open('model_{0:%y%m%d}_{1:%H%M%S}.json'.format(now, now), 'w') as f:
+    #json.dump(model_to_json, f)
+    f.write(model_to_json)
+
+# log として取るもの : (3)
+#   weight (hdf5 形式)
+model.save_weights('weights_{0:%y%m%d}_{1:%H%M%S}.h5'.format(now, now))
+
+# 参考 : https://keras.io/ja/models/about-keras-models/
