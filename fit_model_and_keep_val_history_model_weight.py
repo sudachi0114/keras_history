2289cff877ddabc8d559e4dcbe83a65b6d8b0c44
suzuki daichi <sudachi@DaiMac.local>
2019-07-13 22:03:13 +0900

    出力するものが増えたので、1回ごとに dir を作り出力できるように <fit_model_and_keep_val_history_model_weight.py> を変更した。

--- fit_model_and_keep_val_history_model_weight.py
+++ fit_model_and_keep_val_history_model_weight.py
@@ -47,7 +47,7 @@ history = model.fit(
                         x_train, y_train,
                         batch_size=32,
                         #epochs=10,
-                        epochs=3,
+                        epochs=1,
                         verbose=1,
                         validation_split=0.1
                         #,callbacks=[early_stopping]
@@ -68,26 +68,56 @@ history = model.fit(
 
 # https://qiita.com/karadaharu/items/948e4d313fbaa32e408c
 
-# log として取るもの : (1)
-#   history (history object -> pickle ファイル)
-import pickle, datetime
+
+import datetime
 
 now = datetime.datetime.now()
 
-with open('history_{0:%y%m%d}_{1:%H%M%S}.pkl'.format(now, now), 'wb') as f:
+
+# 以下で取る log を保存する dir を (存在しなければ) 作成 
+#   https://note.nkmk.me/python-save-file-at-new-dir/
+import os
+os.makedirs('outs', exist_ok=True)
+outfile_pass = 'outs/out_{0:%y%m%d}_{1:%H%M%S}'.format(now, now)
+os.makedirs(outfile_pass, exist_ok=True)
+
+""" Python 3.2以降は引数 exist_ok が追加されており、
+        exist_ok=Trueとすると
+            既に末端ディレクトリが存在している場合もエラーが発生しない。
+        末端ディレクトリが存在していなければ新規作成するし、存在していれば何もしない。
+        前もって末端ディレクトリの存在確認をする必要がないので便利。
+"""
+
+""" 新しいディレクトリにファイルを作成・保存する関数のコード例
+def save_file_at_new_dir(new_dir_path, new_filename, new_file_content, mode='w'):
+    os.makedirs(new_dir_path, exist_ok=True)
+    with open(os.path.join(new_dir_path, new_filename), mode) as f:
+        f.write(new_file_content)
+"""
+
+# log として取るもの : (1)
+#   history (history object -> pickle ファイル)
+import pickle
+
+#with open('history_{0:%y%m%d}_{1:%H%M%S}.pkl'.format(now, now), 'wb') as f:
+#with open('./out/history_{0:%y%m%d}_{1:%H%M%S}.pkl'.format(now, now), 'wb') as f:
+with open(os.path.join(outfile_pass, 'history_{0:%y%m%d}_{1:%H%M%S}.pkl'.format(now, now)), 'wb') as f:
     pickle.dump(history, f)
+
 
 # log として取るもの : (2)
 #   model (json 形式)
 import json
 
 model_to_json = model.to_json()
-with open('model_{0:%y%m%d}_{1:%H%M%S}.json'.format(now, now), 'w') as f:
+#with open('model_{0:%y%m%d}_{1:%H%M%S}.json'.format(now, now), 'w') as f:
+with open(os.path.join(outfile_pass, 'model_{0:%y%m%d}_{1:%H%M%S}.json'.format(now, now)), 'w') as f:
     #json.dump(model_to_json, f)
     f.write(model_to_json)
 
 # log として取るもの : (3)
 #   weight (hdf5 形式)
-model.save_weights('weights_{0:%y%m%d}_{1:%H%M%S}.h5'.format(now, now))
+#model.save_weights('weights_{0:%y%m%d}_{1:%H%M%S}.h5').format(now, now)
+model.save_weights(os.path.join(outfile_pass, 'weights_{0:%y%m%d}_{1:%H%M%S}.h5').format(now, now))
 
 # 参考 : https://keras.io/ja/models/about-keras-models/
